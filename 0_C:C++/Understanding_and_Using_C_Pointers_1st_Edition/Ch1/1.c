58e2e124e926a7a44b16821c70f4e2096fec9de5
fbsh <boasfn@gmail.com>
2021-05-02 10:24:43 +0800

    update

---
+++ 0_C:C++/Understanding_and_Using_C_Pointers_1st_Edition/Ch1/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num = 5;
+    const int limit = 500; 
+    int *pi; 
+    const int *pci;
+    pi = &num;
+    pci = &limit;
+
+    printf(" num - Address: %p value: %d\n" ,&num, num);
+    printf("limit - Address: %p value: %d\n" ,&limit, limit);
+    printf(" pi - Address: %p value: %p\n" ,&pi, pi);
+    printf(" pci - Address: %p value: %p\n" ,&pci, pci);
+
+    return 0;
+}
